51fa94483c2d83039806c67dae54d76aff4e5038
Licu Mihai <licu.mihai21@gmail.com>
2022-11-20 00:53:46 +0200

    navbar

--- frontend/src/components/MPPage/MPPage.js
+++ frontend/src/components/MPPage/MPPage.js
@@ -1,21 +1,10 @@
 import React from "react";
+import Navbar from "../navbar/navbar";
+
 function MPPage() {
     return (
         <div>
-            <div class="mpp-header">
-                <div class="container m-auto h-100">
-                    <div class="filter-sort">
-                        <h1>Parl</h1>
-                    </div>
-                    <div class="search-bar">
-
-                    </div>
-                </div>
-            </div>
-
-            <div class="mpp-list">
-
-            </div>
+            <Navbar />
         </div>
     );
 }
