ae488f043b49efdb966ad587bb7cfd669bcc081c
Augustin Cibotari <augustin.cibotari@gmail.com>
2022-11-19 22:08:28 +0200

    first commit frontend

---
+++ frontend/src/components/MPPage/MPPage.js
@@ -0,0 +1,22 @@
+function MPPage() {
+    return (
+        <div>
+            <div class="mpp-header">
+                <div class="container m-auto h-100">
+                    <div class="filter-sort">
+                        
+                    </div>
+                    <div class="search-bar">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="mpp-list">
+
+            </div>
+        </div>
+    );
+}
+
+export default MPPage;
