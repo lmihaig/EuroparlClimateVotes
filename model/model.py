cb4d8e601a9e6c0f482b130fd926ae2d2e92e6fa
Radu Moraru <radu.a.moraru@gmail.com>
2022-11-20 06:38:46 +0200

    Prediction model done.

--- model/model.py
+++ model/model.py
@@ -1,19 +1,18 @@
 import pickle
-import spacy
 
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
 
 
 class Model:
     def __init__(self):
         vocabulary = pickle.load(open('trained_vect.pkl', 'rb'))
         self.vectorizer = CountVectorizer(decode_error='replace', vocabulary=vocabulary)
-        self.nlp = spacy.load('en_core_web_lg')
+        self.nlp = pickle.load(open('nlp.pkl', 'rb'))
         self.model = pickle.load(open('trained_model.pkl', 'rb'))
 
-    def predict(self, x):
-        '''Given text x, return if it is climate change related or not.'''
-        lemmas = []
-        tf_features = self.vectorizer.transform(x)
-        return self.model.predict(x)
+    def predict(self, text):
+        '''Given a text, return if it is climate change related or not.'''
+        processed_text = self.nlp(text)
+        lemmas = ' '.join([token.lemma_ for token in processed_text])
+        tf_features = self.vectorizer.transform([lemmas])
+        return bool(self.model.predict(tf_features)[0])
