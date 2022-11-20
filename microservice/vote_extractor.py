4256b5ce2b3f68b03c8ea09fd21ca220bbab1c7e
Cristi_Sima <cristiansimache@gmail.com>
2022-11-20 03:25:44 +0200

    fixed extractor 2021 data

--- microservice/vote_extractor.py
+++ microservice/vote_extractor.py
@@ -5,8 +5,8 @@ import re
 import threading
 from pprint import pprint
 import xml.etree.ElementTree as ET
-def get_url(day, month, lang="FR"):
-    return f"https://www.europarl.europa.eu/doceo/document/PV-9-2022-{month:02}-{day:02}-RCV_{lang}.xml"
+def get_url(day, month, year=2022, lang="FR"):
+    return f"https://www.europarl.europa.eu/doceo/document/PV-9-{year}-{month:02}-{day:02}-RCV_{lang}.xml"
 
 session = requests.Session()
 
@@ -16,7 +16,7 @@ def rskip_seq(target, seq, count=1): return target.rsplit(seq, count)[0]
 
 amendment_pattertn=re.compile("A[0-9]+-[0-9]+\/[0-9]{4}")
 
-def extract_names(url):
+def extract_names(url, *, session=session):
     resp=session.get(url)
     if resp.status_code != 200:
         if resp.status_code != 404:
@@ -36,12 +36,16 @@ def extract_names(url):
             continue
         # print(description)
         # continue
-        description=description.strip().replace("–", "-").replace("’","'").replace('“', '"').replace('”', '"')
+        description=description.strip().replace("–", "-").replace("’","'").replace('“', '"').replace('”', '"').replace('\n', ' ')
+        if description.count("-")<=1:
+            continue
         am=None
         if description.rsplit("-", 1)[1].startswith(" Am "):
             # print("AAAA")
             description, am =description.rsplit(" - ", 1)
         # print(description)
+        if description.count("-")<=1:
+            continue
         extra_sep_val=0
         # print(description.rsplit(" - ",3)[1])
         if amendment_pattertn.match(description.rsplit(" - ",3)[1]):
@@ -58,6 +62,8 @@ def extract_names(url):
             description_separator, separators),
             description_separator, separators
         )
+        # if am:
+        #     print(description,"#", am)
         returned.append(description)
         # print(description)
         # exit()
@@ -71,30 +77,39 @@ def extract_names(url):
 #         if resp.status_code ==200:
 #             print(day, month)
 
+# print(get_url(15, 9))
 # a=extract_names(get_url(15, 9))
 # a=extract_names(get_url(13, 9))
 # a=extract_names(get_url(5, 7))
 # a=extract_names(get_url(4, 7))
+# a=extract_names(get_url(19, 10, 2021))
 # pprint(a)
 # exit()
 
 
-
+output_lock=threading.Lock()
 output="nume,\n"
 
 def scan_month(month, year):
     global output
+    session = requests.Session()
+    local_output=f""
     for day in range(1,32):
-        for name in extract_names(get_url(day, month)):
-            # print(name)
-            output+=f"{name},\n"
+        for name in extract_names(get_url(day, month, year), session=session):
+            print(day, name)
+            name=name.replace("\"", "'")
+            local_output+=f"\"{name}\",\n"
+    with output_lock:
+        output+=local_output
 
 months=[]
-for year in range(2015, 2023):
+for year in range(2021, 2022):
     for month in range(1, 13):
         thread=threading.Thread(target=scan_month, args=(month, year,))
         thread.start()
         months.append(thread)
+
+# scan_month(10, 2021)
 
 list(map(threading.Thread.join, months))
 
